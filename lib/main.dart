60bb970a23f0f374a496fb165b7353aa8e90c9e2
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-13 14:07:59 +0200

    ci: create routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:afri_fair/screens/home.dart';
+import 'package:afri_fair/screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,52 +18,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomePage()
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Center(child: Text(widget.title, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent),)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(),
+          '/login': (context) => LoginPage()
+        },
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,10 +11,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
-          mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
             Image.asset('assets/home_image.png'),
             Column(
               spacing: 30,
@@ -30,13 +30,26 @@ class HomePage extends StatelessWidget {
                 ),
                 ElevatedButton(
                   style: ButtonStyle(
-                    backgroundColor:  WidgetStateProperty.all(AppColors.white),
-                    padding: WidgetStateProperty.all(EdgeInsets.symmetric(vertical: 10, horizontal: 18))
+                    backgroundColor: WidgetStateProperty.all(AppColors.white),
+                    padding: WidgetStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 18),
+                    ),
                   ),
-                    onPressed: (){}, child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                  child: Text('Get Started', style: TextStyle(color: AppColors.black ,fontSize: 16, fontWeight: FontWeight.w500),),
-                )),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: AppColors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
 
