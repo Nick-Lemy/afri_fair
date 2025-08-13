890e5e15f9419df43d50265311a597848986eaf3
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-13 14:20:42 +0200

    ci: implementing the design step by step

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -12,13 +12,27 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Welcome to AfriFair !',
-            style: TextStyle(color: AppColors.black, fontSize: 30),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            spacing: 32,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to AfriFair !',
+                style: TextStyle(color: AppColors.black, fontSize: 30, fontWeight: FontWeight.w600),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 250),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: AppColors.purpleAccented,
+                ),
+              )
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
